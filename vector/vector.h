b45f9694edbdd930344ab85e41f9266aecc9642a
ol1xy <98576934+ol1xy@users.noreply.github.com>
2024-05-03 16:50:39 +0800

    Update vector.h

--- vector/vector.h
+++ vector/vector.h
@@ -3,56 +3,66 @@
 
 template <typename T>
 class Vector {
-    private:
-        T* array; // Указатель на первый элемент массива
-        size_t capacity; // Размер выделенной памяти
-        size_t size; // Текущий размер вектора
 
-    public:
-        // Конструктор по умолчанию
-        Vector() : array(nullptr), capacity(0), size(0) {}
+private:
+    T* array; // Указатель на первый элемент массива
+    size_t capacity; // Размер выделенной памяти
+    size_t size; // Текущий размер вектора
 
-        // Конструктор с заданным начальным размером
-        Vector(size_t initial_size) : array(new T[initial_size]), capacity(initial_size), size(initial_size) {}
+public:
+    Vector(); // Конструктор по умолчанию
+    Vector(size_t initial_size); // Конструктор с заданным начальным размером
+    ~Vector(); // Деструктор
 
-        // Деструктор
-        ~Vector() {
-            delete[] array;
-        }
+    void push_back(const T& value); // Метод добавления элемента в конец вектора
+    void erase(size_t index); // Метод удаления элемента из указанной позиции
+    void insert(size_t index, const T& value); // Метод добавления элемента в указанную позицию
+    const T& operator[](size_t index) const; // Проверка индекса
+    size_t getSize() const;  // Метод получения размера вектора
+    void reserve(size_t new_capacity);  // Метод выделения памяти для вектора   
 
-    // Метод добавления элемента в конец вектора
-    void push_back(const T& value) {
-        // Если массив уже заполнен, увеличиваем его размер вдвое
+    // Оператор вывода
+    friend std::ostream& operator<<(std::ostream& out, const Vector<T>& vec);
+};
+
+    template <typename T>
+    Vector<T>::Vector() : array(nullptr), capacity(0), size(0) {}
+
+    template <typename T>
+    Vector<T>::Vector(size_t initial_size) : array(new T[initial_size]), capacity(initial_size), size(initial_size) {}
+
+    template <typename T>
+    Vector<T>::~Vector() {
+        delete[] array;
+    }
+
+    template <typename T>
+    void Vector<T>::push_back(const T& value) {
         if (size >= capacity) {
             reserve(capacity == 0 ? 1 : capacity * 2);
         }
         array[size++] = value;
     }
 
-    // Метод удаления элемента из указанной позиции
-    void erase(size_t index) {
+    template <typename T>
+    void Vector<T>::erase(size_t index) {
         if (index >= size) {
             throw std::out_of_range("Индекс выходит за пределы вектора");
         }
-        // Сдвигаем элементы влево начиная с индекса до конца массива
         for (size_t i = index; i < size - 1; ++i) {
             array[i] = array[i + 1];
         }
         --size;
     }
 
-    // Метод добавления элемента в указанную позицию
-    void insert(size_t index, const T& value) {
+    template <typename T>
+    void Vector<T>::insert(size_t index, const T& value) {
         if (index > size) {
             throw std::out_of_range("Индекс выходит за пределы вектора");
         }
-
-        // Если массив уже заполнен, увеличиваем его размер вдвое
         if (size >= capacity) {
             reserve(capacity == 0 ? 1 : capacity * 2);
         }
-
-        // Сдвигаем элементы вправо начиная с конца массива до индекса
         for (size_t i = size; i > index; --i) {
             array[i] = array[i - 1];
         }
@@ -60,21 +70,21 @@ class Vector {
         ++size;
     }
 
-    const T& operator[](size_t index) const {
+    template <typename T>
+    const T& Vector<T>::operator[](size_t index) const {
         if (index >= size) {
             throw std::out_of_range("Индекс выходит за пределы вектора");
         }
-
         return *(array + index);
     }
 
-    // Метод получения размера вектора
-    size_t getSize() const {
+    template <typename T>
+    size_t Vector<T>::getSize() const {
         return size;
     }
 
-    // Метод выделения памяти для вектора
-    void reserve(size_t new_capacity) {
+    template <typename T>
+    void Vector<T>::reserve(size_t new_capacity) {
         if (new_capacity <= capacity) {
             return;
         }
@@ -87,8 +97,8 @@ class Vector {
         capacity = new_capacity;
     }
 
-    // Оператор вывода для класса Vector
-    friend std::ostream& operator<<(std::ostream& out, const Vector<T>& vec) {
+    template <typename T>
+    std::ostream& operator<<(std::ostream& out, const Vector<T>& vec) {
         out << "[";
         for (size_t i = 0; i < vec.size; ++i) {
             out << vec.array[i];
@@ -98,5 +108,4 @@ class Vector {
         }
         out << "]";
         return out;
-    }
-};
+    };
